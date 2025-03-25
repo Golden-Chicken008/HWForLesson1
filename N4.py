a4f8c4bf3e98c45e0265a0666e0ff92e53c28e97
Golden-Chicken008 <chicken008@mail.ru>
2025-03-25 10:56:39 +0300

    Второе изменение (небольшое)
    Исправил то, что PyCharm автоматически импортировал count из-за того, что я записал это в качестве переменной + я немного подправил сообщение об ошибке.

--- N4.py
+++ N4.py
@@ -1,17 +1,15 @@
-from itertools import count
-
 while True:
     number = input("Введите число: → ")
     if number == "exit":
         print("Выход из программы...")
         break
     elif not number.isdigit() and not number.lstrip('-').isdigit():
-        print("Ошибка: данные не являются числом.")
+        print("Ошибка: данные не являются целым числом.")
     elif number.isdigit():
-        count = len(number)
-        print(f"В этом числе {count} цифры.")
+        number_of_digits = len(number)
+        print(f"В этом числе {number_of_digits} цифры.")
     elif not number.isdigit() and number.lstrip('-').isdigit():
-        count = len(number.lstrip('-'))
-        print(f"В этом числе {count} цифры.")
+        number_of_digits = len(number.lstrip('-'))
+        print(f"В этом числе {number_of_digits} цифры.")
     else:
         print("Ошибка: программист не додумался, как получить это сообщение")
